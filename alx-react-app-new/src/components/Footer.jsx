52b370b76038b8cffc5eb63ca3f97503e0eb6136
Abdullahi <adetunjiabdullahi2406@gmail.com>
2026-03-06 23:28:03 +0100

    update footer

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer style={{ borderTop: '1px solid #ccc', marginTop: '20px', textAlign: 'right', color: 'gray' }}>
-            <p>© 2026 Tralblazer</p>
+            <p>© 2026 Trailblazer</p>
         </footer>
     );
 }
